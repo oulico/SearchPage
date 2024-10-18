7204106bdca9d5becff246ca51cfcb929eace126
JH <13332150+oulico@users.noreply.github.com>
2024-10-18 13:23:48 +0900

    HOTFIX console.log 추가

--- src/utils/getBaseURL.ts
+++ src/utils/getBaseURL.ts
@@ -5,10 +5,24 @@ import {isVercel} from "utils/isVercel"
 
 export const getBaseURL = () => {
     if (isVercel()) {
+        console.log('isVercel')
+        console.log(process.env.VERCEL_URL)
+        console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
         return `https://${process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL}`;
     } else if (isLocal()) {
+        console.log('isLocal')
+        console.log(process.env.VERCEL_URL)
+        console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
         return `http://localhost:3000`
     } else if (isProd()) {
+        console.log('isProd')
+        console.log(process.env.VERCEL_URL)
+        console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
         return `https://${HOST}`
+    } else {
+        console.log('else')
+        console.log(process.env.VERCEL_URL)
+        console.log(process.env.NEXT_PUBLIC_VERCEL_URL)
+        return `https://${process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL}`
     }
 }
