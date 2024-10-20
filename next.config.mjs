6590588fc778bc6ba225f31429d81dfec64b2c90
JH <13332150+oulico@users.noreply.github.com>
2024-10-20 10:15:20 +0900

    HOTFIX:seo

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,21 @@ const nextConfig = {
         return config;
     },
 
+    async headers() {
+        const headers = [];
+        if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+            headers.push({
+                headers: [
+                    {
+                        key: 'X-Robots-Tag',
+                        value: 'noindex',
+                    },
+                ],
+                source: '/:path*',
+            });
+        }
+        return headers;
+    },
 };
 
 export default nextConfig;
